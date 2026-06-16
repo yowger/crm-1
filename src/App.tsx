b3e8db4d03eb5f3e4a00bec3f20ec9605415abec
Ver0018 <rogerpantilyowger@gmail.com>
2026-06-17 02:37:58 +0800

    clean up base

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,51 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core"
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools"
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar"
 
-import { useNotificationProvider } from "@refinedev/antd";
-import "@refinedev/antd/dist/reset.css";
+import { useNotificationProvider } from "@refinedev/antd"
+import "@refinedev/antd/dist/reset.css"
 
-import dataProvider, {
-  GraphQLClient,
-  liveProvider,
-} from "@refinedev/nestjs-query";
+// import dataProvider, { liveProvider } from "@refinedev/nestjs-query"
 import routerProvider, {
-  DocumentTitleHandler,
-  UnsavedChangesNotifier,
-} from "@refinedev/react-router";
-import { App as AntdApp } from "antd";
-import { createClient } from "graphql-ws";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { ColorModeContextProvider } from "./contexts/color-mode";
-import { authProvider } from "./providers/auth";
-
-const API_URL = "https://api.nestjs-query.refine.dev/graphql";
-const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
-
-const gqlClient = new GraphQLClient(API_URL);
-const wsClient = createClient({ url: WS_URL });
+    DocumentTitleHandler,
+    UnsavedChangesNotifier,
+} from "@refinedev/react-router"
+import { App as AntdApp } from "antd"
+import { BrowserRouter, Route, Routes } from "react-router"
 
 function App() {
-  return (
-    <BrowserRouter>
-      <GitHubBanner />
-      <RefineKbarProvider>
-        <ColorModeContextProvider>
-          <AntdApp>
-            <DevtoolsProvider>
-              <Refine
-                dataProvider={dataProvider(gqlClient)}
-                liveProvider={liveProvider(wsClient)}
-                notificationProvider={useNotificationProvider}
-                routerProvider={routerProvider}
-                authProvider={authProvider}
-                options={{
-                  syncWithLocation: true,
-                  warnWhenUnsavedChanges: true,
-                  projectId: "InBoqM-aW2aYw-ONAijf",
-                  liveMode: "auto",
-                }}
-              >
-                <Routes>
-                  <Route index element={<WelcomePage />} />
-                </Routes>
-                <RefineKbar />
-                <UnsavedChangesNotifier />
-                <DocumentTitleHandler />
-              </Refine>
-              <DevtoolsPanel />
-            </DevtoolsProvider>
-          </AntdApp>
-        </ColorModeContextProvider>
-      </RefineKbarProvider>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <GitHubBanner />
+            <RefineKbarProvider>
+                <AntdApp>
+                    <DevtoolsProvider>
+                        <Refine
+                            // dataProvider={dataProvider(gqlClient)}
+                            // liveProvider={liveProvider(wsClient)}
+                            notificationProvider={useNotificationProvider}
+                            routerProvider={routerProvider}
+                            // authProvider={authProvider}
+                            options={{
+                                syncWithLocation: true,
+                                warnWhenUnsavedChanges: true,
+                                projectId: "InBoqM-aW2aYw-ONAijf",
+                                liveMode: "auto",
+                            }}
+                        >
+                            <Routes>
+                                <Route index element={<WelcomePage />} />
+                            </Routes>
+                            <RefineKbar />
+                            <UnsavedChangesNotifier />
+                            <DocumentTitleHandler />
+                        </Refine>
+                        <DevtoolsPanel />
+                    </DevtoolsProvider>
+                </AntdApp>
+            </RefineKbarProvider>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
