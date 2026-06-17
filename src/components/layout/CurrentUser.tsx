a16fb4192707e07bdeb14ec4cfca644db3bba84e
Ver0018 <rogerpantilyowger@gmail.com>
2026-06-18 02:49:38 +0800

    added drop down menu

--- src/components/layout/CurrentUser.tsx
+++ src/components/layout/CurrentUser.tsx
@@ -1,4 +1,4 @@
-import { Popover } from "antd"
+import { Dropdown, MenuProps } from "antd"
 import { useGetIdentity } from "@refinedev/core"
 
 import { CustomAvatar } from "../CustomAvatar"
@@ -7,14 +7,39 @@ import type { UserIdentity } from "../../types/auth"
 export const CurrentUser = () => {
     const { data: user } = useGetIdentity<UserIdentity>()
 
+    const items: MenuProps["items"] = [
+        {
+            key: "user",
+            label: (
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                    <span style={{ fontWeight: 500 }}>{user?.name}</span>
+                    <span style={{ fontSize: 12, opacity: 0.6 }}>
+                        {user?.email}
+                    </span>
+                </div>
+            ),
+        },
+        {
+            type: "divider",
+        },
+        {
+            key: "settings",
+            label: "Settings",
+        },
+        {
+            key: "profile",
+            label: "Profile",
+        },
+    ]
+
     return (
-        <Popover placement="bottomRight" trigger="click">
+        <Dropdown menu={{ items }} trigger={["click"]}>
             <CustomAvatar
                 name={user?.name ?? "User"}
                 src={user?.avatar}
                 size="default"
                 style={{ cursor: "pointer" }}
             />
-        </Popover>
+        </Dropdown>
     )
 }
